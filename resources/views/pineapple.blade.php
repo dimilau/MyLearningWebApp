d7d41317795712feb79297237b6ce0711c013788
Dimi Lau <dimi.lsw@gmail.com>
2025-07-30 08:32:57 +0800

    Implement route and view. Pass in variable to view.

---
+++ resources/views/pineapple.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <head></head>
+    <body>
+        <h1>{{ $name }}</h1>
+        <p>This is the Pineapple page.</p>
+        <p>Welcome to the Pineapple page!</p>
+
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pineapple', function () {
+    return view('pineapple', ['name' => 'Pineapple']);
+});
